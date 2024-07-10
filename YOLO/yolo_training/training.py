a575ce8545d4553aa62816c34d75e34bf9e89ed0
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2024-07-10 16:57:59 +0900

    まっすぐなよろ
    実はカラーコーンのスペル間違えてる

---
+++ YOLO/yolo_training/training.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+# クラス数1のYOLOv10nのyamlファイルをロード
+# model = YOLO("C:/Users/hiyos/OneDrive - 埼玉大学/あかとき/プログラム/SC-21/dataset/class1_yolov10n.yaml")
+
+# YOLOv10nのptファイルをロード
+model = YOLO("yolov10n.pt")
+
+# モデルをyamlファイルでトレーニング
+results = model.train(data="C:/Users/hiyos/OneDrive - 埼玉大学/あかとき/プログラム/SC-21/dataset/training.yaml", epochs=100, imgsz=640)
+
+# モデルをエクスポート
+# model.save("yolo_20240709_training.pt")
+
+
+
+
