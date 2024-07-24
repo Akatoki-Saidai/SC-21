2cdee8d049d0419930fa71153d3f1ee2d20e6e30
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2024-07-24 19:38:53 +0900

    冗長性(笑)
    三回も測定してるよ

--- bbm/memory_leak/memory_leak.py
+++ bbm/memory_leak/memory_leak.py
@@ -1,13 +1,15 @@
+# BMPとBNO(とGPS)を受信してメモリリーク"する"プログラム
+
+
+import gc
+import psutil
 import time
 from smbus2 import SMBus
 import serial
 from gpiozero import LED
-from picamera2 import Picamera2
 
 # scに使用ライブラリほぼまとめました
-from sc import motor
 import sc.print_override as override
-from sc.camera import Camera
 from sc.bmp280 import BMP280
 from sc.bno055 import BNO055
 from sc.micropyGPS import MicropyGPS
@@ -46,19 +48,6 @@ def main():
         print(f"An error occured in turn on bmp, bno, led: {e}")
 
 
-    # モータードライバセットアップ
-    try:
-        PIN_AIN1 = 18
-        PIN_AIN2 = 23
-        PIN_BIN1 = 24
-        PIN_BIN2 = 13
-
-        motor_right, motor_left = motor.setup(PIN_AIN1, PIN_AIN2, PIN_BIN1, PIN_BIN2)
-
-    except Exception as e:
-        print(f"An error occured in setting motor_driver: {e}")
-
-
     # gpsのセットアップ
     try:
         gnss = MicropyGPS(9, 'dd')
@@ -82,6 +71,9 @@ def main():
     try:
         bus = SMBus(1)
         bmp280 = BMP280(i2c_dev=bus)
+
+        temperature = []
+        pressure = []
     except Exception as e:
             print(f"An error occured in setting bmp280 object: {e}")
 
@@ -98,18 +90,6 @@ def main():
     except Exception as e:
             print(f"An error occured in getting bmp280 data: {e}")
 
-    # カメラセットアップ
-    try:
-        CameraStart = False
-        picam2 = Picamera2()
-        config = picam2.create_preview_configuration({"format": 'XRGB8888', "size": (320, 240)})
-        picam2.configure(config)
-        cam = Camera()
-
-    except Exception as e:
-        print(f"An error occurred in init camera: {e}")
-
-
 
     # ---繰り返しゾーン---
 
@@ -122,35 +102,6 @@ def main():
                 print(f"An error occured in getting data from serial 0: {e}")
                 # print(len(sentence))
                 # continue
-
-        try:
-            # モーターを回転して前進
-            motor.accel(motor_right, motor_left)
-            print("motor: forward -1s")
-            time.sleep(1)  # 何秒進むか
-
-            # モーターの回転を停止
-            motor.brake(motor_right, motor_left)
-            print("motor: brake")
-            time.sleep(1)  # 何秒進むか
-
-            # モーターを回転させ，CanSatを1秒くらい右回転
-            motor.rightturn(motor_right, motor_left)
-            print("motor: rightturn")
-
-            # モーターを回転させ，CanSatを1秒くらい左回転
-            motor.leftturn(motor_right, motor_left)
-            print("motor: leftturn")
-            
-            
-            time.sleep(1)
-
-        except Exception as e:
-            print(f"An error occured in moving motor: {e}")
-            # 停止
-            motor_left.value = 0.0
-            motor_right.value = 0.0
-
 
         # GPS緯度経度読み取り
         try:
@@ -195,34 +146,36 @@ def main():
     
         # bmp280データ取得(基準高度参照)
         try:
-            temperature, pressure = bmp280.get_temp_pres()
+            temperature = bmp280.get_temperature()
+            pressure = bmp280.get_pressure()
             altitude = bmp280.get_altitude(qnh=baseline)
             print(f"temperture{temperature:05.2f}*C")
             print(f"pressure: {pressure:05.2f}hPa")
             print(f"Relative altitude: {altitude:05.2f} metres")
+            
+            
+            
+            # ここ決してマネしないでください
+            temperature.append(pressure)
+            pressure.append(temperature)
+
         except Exception as e:
             print(f"An error occured in reading bmp280: {e}")
 
 
-        # カメラデータ取得&処理
         try:
-            # フレームを取得
-            if (CameraStart == False):
-                picam2.start()
-                CameraStart = True
-            if (CameraStart == True):
-                frame = picam2.capture_array()
-                # 赤色を検出
-                mask = cam.red_detect(frame)
-                # 面積のもっとも大きい領域を表示
-                camera_order = cam.analyze_red(frame, mask)
-                # 結果表示
-                time.sleep(0.5)
-                #print(len(contours))
+            
+            for i in range(100):
+                memory_state = psutil.virtual_memory()
+                print("memory: ", memory_state.percent)
+                time.sleep(0.1)
+
+            gc.collect()
+            memory_state = psutil.virtual_memory()
+            print("memory: ", memory_state.percent)
 
         except Exception as e:
-            print(f"An error occured in processing camera: {e}")
-
+             print(f"An error occured in surveying memoryleak: {e}")
 
 
 
