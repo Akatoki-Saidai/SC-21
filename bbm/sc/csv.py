ca0ce347e511009e98b282d43ebbacc5d5816825
n17y17 <N17Y17@outlook.com>
2024-07-24 19:00:03 +0900

    保存場所間違えた

--- bbm/sc/csv.py
+++ bbm/sc/csv.py
@@ -1,5 +1,21 @@
-# csvの書き込みやる(まだ書けてません)
+# csvの書き込み
 
-class csvrite:
-    def write():
-        a = a
+import time
+import copy
+
+msg_types = ['monotonic', 'error', 'warning', 'msg', 'fase', 'alt', 'temp', 'press', 'accel_all_x', 'accel_all_y', 'accel_all_z', 'accel_x', 'accel_y', 'accel_z', 'mag_x', 'mag_y', 'mag_z', 'gyro_x', 'gyro_y', 'gyro_z', 'time', 'date']
+DEFAULT_DICT = {x : '' for x in msg_types}
+
+def print(msg_type : str, msg_data):
+    output_dict = copy.deepcopy(DEFAULT_DICT)
+    if (msg_type == 'accel_all') or (msg_type == 'accel') or (msg_type == 'mag') or (msg_type == 'gyro'):
+        output_dict[msg_type + '_x'] = str(msg_data[0])
+        output_dict[msg_type + '_y'] = str(msg_data[1])
+        output_dict[msg_type + '_z'] = str(msg_data[2])
+    else:
+        output_dict[msg_type] = str(msg_data)
+    output_dict['monotonic'] = str(time.monotonic_ns())
+    output_msg = ','.join(output_dict.values())
+    print(output_msg)
+    with open('csv_log_test.txt', 'a') as f:
+        f.write(output_msg + '\n')
